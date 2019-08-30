6012dcc987a04c44795f6733c983cc12148b64c7
sstelss <32020666+sstelss@users.noreply.github.com>
2019-08-30 21:14:50 +0300

    Add files via upload

---
+++ src/server/db.js
@@ -0,0 +1,7 @@
+// module.exports = {
+//     'url' : 'mongodb://localhost:27017/pokedex'
+// }
+
+module.exports = {
+    'url' : 'mongodb+srv://admin:admin@pokedex-4iocn.mongodb.net/test?retryWrites=true&w=majority'
+}
